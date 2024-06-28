218c61ed00b3380b24d6e404ccadadbaac5a3089
Rui <yehruei@163.com>
2024-06-28 17:58:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: '/original-photoshop/', // 替换为您的仓库名称
+  build: {
+    outDir: 'dist'
+  }
+});
